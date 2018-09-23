6e93ede7dd603e9b1e881c0fd1c0f99c24d91091
Alex <akrim777@gmail.com>
2018-09-23 19:18:58 +0100

    implemented updating the number of images in album with cloud functions

--- Firestore/functions/index.js
+++ Firestore/functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
+
+exports.imageAdded = functions.firestore
+    .document('images/{imageId}')
+    .onCreate(updatePhotosCount);
+
+exports.imageDeleted = functions.firestore
+    .document('images/{imageId}')
+    .onDelete(updatePhotosCount);
+
+function updatePhotosCount(snapshot) {
+    const albumId = snapshot.data().albumId;
+
+    return db.collection('images').where('albumId', '==', albumId).get()
+        .then(snapshot => {
+            return snapshot.docs.length || 0;
+        }).then(count => {
+            return db.collection('albums').doc(albumId).update({ 'numberOfPhotos': count });
+        });
+}
