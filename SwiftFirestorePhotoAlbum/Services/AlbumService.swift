b394eee7372fb11626fa3ba1980ea09978ebf40d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-12 12:55:16 +0100

    test

--- SwiftFirestorePhotoAlbum/Services/AlbumService.swift
+++ SwiftFirestorePhotoAlbum/Services/AlbumService.swift
@@ -2,8 +2,7 @@
 //  AlbumService.swift
 //  SwiftFirestorePhotoAlbum
 //
-//  Created by Alex Akrimpai on 03/09/2018.
-//  Copyright © 2018 Alex Akrimpai. All rights reserved.
+//  
 //
 
 import Foundation
